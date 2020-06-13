73c9d0d4651945651f5ac150bb6b2a0117fca328
Minseok Song <21700372@handong.edu>
2020-06-13 01:52:22 +0100

    files uploaded

---
+++ cctvFor5Sec.py
@@ -0,0 +1,11 @@
+from picamera import PiCamera
+from time import sleep
+import time
+import datetime
+
+camera = PiCamera()
+now = datetime.datetime.now()
+filename = now.strftime('%Y-%m-%d %H:%M:%S')
+camera.start_recording(output = '/home/pi/picam/' + filename + '.h264')
+sleep(5)
+camera.stop_recording()
